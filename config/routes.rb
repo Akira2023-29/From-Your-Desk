df203e3dc832fc66654aa9e55e707e89e5b04a87
アキラ <akisasa1994@gmail.com>
2023-11-28 09:49:45 +0900

    Topページのベース完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'top_pages/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "top_pages#top"
 end
