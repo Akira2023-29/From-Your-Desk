444d6cd9edeae4c6b812d1aaf706079b30d1f241
アキラ <akisasa1994@gmail.com>
2023-12-01 16:07:58 +0900

    entrypoint.sh修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rails db:migrate RAILS_ENV=production
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
