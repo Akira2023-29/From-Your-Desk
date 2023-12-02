40baf59e7f8550451b57d154e589697d8c974209
アキラ <akisasa1994@gmail.com>
2023-12-02 17:15:28 +0900

    プリコンパイル設定テスト

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
-# bundle exec rails assets:precompile RAILS_ENV=production
+bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rails db:migrate RAILS_ENV=production
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
