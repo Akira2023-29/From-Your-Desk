4b51933243bd7cafbe7cdd9db006db1a7e87b659
アキラ <akisasa1994@gmail.com>
2023-12-04 11:38:53 +0900

    ユーザ-詳細画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
 
   root 'top_pages#top'
   resources :users, only: %i[new create]
+
+  resource :profile, only: %i[show edit update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
