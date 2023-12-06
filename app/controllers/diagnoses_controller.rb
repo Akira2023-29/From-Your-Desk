537dcbd2360dc27e09429775d6eea3943bb1f43d
Akira <akisasa1994@gmail.com>
2023-12-06 15:57:11 +0900

    createアクション設定

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -2,6 +2,7 @@ class DiagnosesController < ApplicationController
   skip_before_action :require_login, only: %i[index show]
 
   def index
+    @diagnosis = Diagnosis.all
   end
 
   def new
@@ -15,6 +16,28 @@ class DiagnosesController < ApplicationController
   end
 
   def create
+    @diagnosis = Diagnosis.new(diagnosis_params)
+
+    # 解析結果をresultカラムにセット。
+    uploaded_image_path = params[:diagnosis][:desk_image].tempfile.path
+    analysis_result = @diagnosis.analyze_image(uploaded_image_path)
+    @diagnosis.result = JSON.dump(analysis_result) if analysis_result.present?
+
+    if analysis_result.present?
+      # 解析結果をマップして指定の形式に変換。
+      @diagnosis.result = analysis_result.map do |item|
+        # RGB値の値を抽出。
+        color = item[:color].split(', ').map(&:to_i).join(", ")
+        # 各色の全ピクセル数に対するピクセル数の比率を抽出（%)
+        pixel_fraction = (item[:pixelFraction]).round(1)
+        "#{color}: #{pixel_fraction}%"
+      end
+      # データベースに保存
+      @diagnosis.save
+    else
+      flash.now[:danger] = '画像を解析できませんでした。'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,5 +1,7 @@
 class Diagnosis < ApplicationRecord
     mount_uploader :desk_image, DeskImageUploader 
-    
+
     belongs_to :user
+
+    validates :result, presence: true, length: { maximum: 200 }
 end
