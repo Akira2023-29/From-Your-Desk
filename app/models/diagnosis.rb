939db22d92a013fcf1f6212662722ca62d3819ab
Akira <akisasa1994@gmail.com>
2023-12-19 17:04:49 +0900

    userモデル、diagnosisモデルにアソシエーション追加等

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -6,6 +6,8 @@ class Diagnosis < ApplicationRecord
     has_many :diagnosis_tags
     has_many :tags, through: :diagnosis_tags
 
+    has_maby :favorites, depentent: :destroy
+
     # validates :desk_image, presence: true, length: { maximum: 200 }
     # validates :color_info, presence: true, length: { maximum: 200 }
 end
