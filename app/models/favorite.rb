e46a45c476589a7497d05c37fe3bf132017d082d
Akira <akisasa1994@gmail.com>
2023-12-20 14:13:50 +0900

    タイポ修正。エラー解決。

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :diagnosis
 
-  varidates :user_id, uniqueness: { scope: :diagnosis_id }
+  validates :user_id, uniqueness: { scope: :diagnosis_id }
 end
