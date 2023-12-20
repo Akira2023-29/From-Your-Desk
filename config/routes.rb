00d574cf5d113cc66fb2341d323f09896c042e58
Akira <akisasa1994@gmail.com>
2023-12-20 11:34:38 +0900

    favoritesルーティングをcollectionルーティングで追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@
 
 Rails.application.routes.draw do
   root 'top_pages#top'
+
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
 
   resources :diagnoses, only: %i[index show new create destroy] do
     get 'tagged', on: :collection
+    get 'favorites', on: :collection
   end
+
+  resources :favorites, only: %i[create destroy]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
