c2630b7d1a0f66bc5b748aa3d0e0dd60de13dc0d
Akira <akisasa1994@gmail.com>
2023-12-20 14:44:44 +0900

    お気に入り機能をturbo_streamで非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    diagnosis = Diagnosis.find(params[:diagnosis_id])
-    current_user.favorite(diagnosis)
+    @diagnosis = Diagnosis.find(params[:diagnosis_id])
+    current_user.favorite(@diagnosis)
   end
 
   def destroy
-    diagnosis = current_user.favorites.find(params[:id]).diagnosis
-    current_user.unfavorite(diagnosis)
+    @diagnosis = current_user.favorites.find(params[:id]).diagnosis
+    current_user.unfavorite(@diagnosis)
   end
 end
