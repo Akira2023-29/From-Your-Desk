ae4775e2a71d8ebe729458eff3137942396e4374
Akira <akisasa1994@gmail.com>
2023-12-21 18:31:02 +0900

    css修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+    def default_meta_tags
+        {
+            site: 'Desk From Cokor',
+            title: 'デスク環境診断サービス',
+            reverse: 'true',
+            charset: 'utf-8',
+            description: 'デスク環境を色彩の観点から診断し、作業効率を上げるためのアドバイスを提案します。',
+            
+        }
+    end
 end
