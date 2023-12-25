9b65a41943f2637dc7b08cafa3ba11e222d040a6
Akira <akisasa1994@gmail.com>
2023-12-25 10:55:28 +0900

    avater_imageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_19_074632) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_25_015402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_19_074632) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
