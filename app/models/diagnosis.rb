c8abdbda0dd086e60a8b84a3e4d3be1751897ae4
Akira <akisasa1994@gmail.com>
2023-12-26 09:43:47 +0900

    desk_workカラム追加・openaiに渡す

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,13 +1,12 @@
 class Diagnosis < ApplicationRecord
     mount_uploader :desk_image, DeskImageUploader 
 
+    validates :desk_work, presence: true, length: { maximum: 255 }
+
     belongs_to :user
 
     has_many :diagnosis_tags, dependent: :destroy
     has_many :tags, through: :diagnosis_tags
 
     has_many :favorites, dependent: :destroy
-
-    # validates :desk_image, presence: true, length: { maximum: 200 }
-    # validates :color_info, presence: true, length: { maximum: 200 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_25_015402) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_25_081300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_25_015402) do
     t.string "desk_image"
     t.text "color_info"
     t.string "color_name"
+    t.string "desk_work"
     t.index ["user_id"], name: "index_diagnoses_on_user_id"
   end
 
