481393561e2a944cb30ab1efdaabf5bb3c00134c
Akira <akisasa1994@gmail.com>
2023-12-27 13:20:51 +0900

    ログイン後のリダイレクト先をトップから一覧画面に変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path, success: 'ログインに成功しました！'
+      redirect_to diagnoses_path, success: 'ログインに成功しました！'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
