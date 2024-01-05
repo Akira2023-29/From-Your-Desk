e47990ff8945ef6e958181428c6c431a5a6fed59
Akira <akisasa1994@gmail.com>
2024-01-06 00:19:23 +0900

    デプロイテスト再6

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # デプロイ時にプリコンパイル、データベースのマイグレーション実行。
-# if [ "$RAILS_ENV" = "production" ]; then
-#   bundle exec rails assets:precompile
-#   bundle exec rails db:migrate
-# fi
+if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rails assets:precompile
+  bundle exec rails db:migrate
+fi
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
