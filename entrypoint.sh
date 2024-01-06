ad7fedec9f4c9d65a722517b404b551aa85f2e0a
Akira <akisasa1994@gmail.com>
2024-01-06 16:32:16 +0900

    プロフィール詳細画面に診断履歴表示

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ rm -f /app/tmp/pids/server.pid
 
 # デプロイ時にプリコンパイル、データベースのマイグレーション実行。
 if [ "$RAILS_ENV" = "production" ]; then
+  # bundle exec rails assets:clobber
   bundle exec rails assets:precompile
   bundle exec rails db:migrate
 fi
