c8d37625ff6de7eb4850061d6b376f95b4de1fa9
Akira <akisasa1994@gmail.com>
2024-01-10 14:22:36 +0900

    s3設定変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,10 +16,11 @@ CarrierWave.configure do |config|
             region: 'ap-northeast-1'                                    # AWSリージョン設定（ここでは東京リージョンを指定）
         }
 
+        config.storage = :fog
         config.fog_directory  = 'from-desk-color'                                        # アップロードされたファイルを保存するS3のバケット（ディレクトリ）名
         config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/from-desk-color'    # ファイルへのURLが生成される際のベースURL
         config.cache_storage = :fog                                                      # アップロード前のキャッシュストレージとしてS3を使用することを指定。これにより、アップロード前の一時的なファイルもS3上に保存される。
-        config.fog_public = true                                                         # ファイルへのアクセスを公開。falseに設定すると、生成されるURLは署名付きURLとなり、一定時間経過後にアクセスできなくなる。
+        config.fog_public = false                                                        # ファイルへのアクセスを公開。falseに設定すると、生成されるURLは署名付きURLとなり、一定時間経過後にアクセスできなくなる。
         config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }    # S3にアップロードされるファイルに対するHTTPヘッダのデフォルト設定。ここでは、cache_controlヘッダを設定して、ブラウザがキャッシュをどの程度の期間保持すべきかを指示している。ここでは最大365日間キャッシュを保持するように設定。
     end
 end
