55fc029fd9daaf6b0c0502cea3bb88357b3df49b
Akira <akisasa1994@gmail.com>
2024-01-10 10:40:39 +0900

    carrierwaveのS3設定ファイル準備

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,15 @@
 CarrierWave.configure do |config|
+    # Amazon S3に接続するための認証情報
     config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
-      aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
-      use_iam_profile:       true,                         # optional, defaults to false
-      region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-      host:                  's3.example.com',             # optional, defaults to nil
-      endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+        provider: 'AWS',
+        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: 'ap-northeast-1'                                    # AWSリージョン設定（ここでは東京リージョンを指定）
     }
-    config.fog_directory  = 'name_of_bucket'                                      # required
-    config.fog_public     = false                                                 # optional, defaults to true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-    # For an application which utilizes multiple servers but does not need caches persisted across requests,
-    # uncomment the line :file instead of the default :storage.  Otherwise, it will use AWS as the temp cache store.
-    # config.cache_storage = :file
-  end
+
+    config.fog_directory  = 'from-desk-color'                                        # アップロードされたファイルを保存するS3のバケット（ディレクトリ）名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/from-desk-color'    # ファイルへのURLが生成される際のベースURL
+    config.cache_storage = :fog                                                      # アップロード前のキャッシュストレージとしてS3を使用することを指定。これにより、アップロード前の一時的なファイルもS3上に保存される。
+    config.fog_public = true                                                         # ファイルへのアクセスを公開。falseに設定すると、生成されるURLは署名付きURLとなり、一定時間経過後にアクセスできなくなる。
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }    # S3にアップロードされるファイルに対するHTTPヘッダのデフォルト設定。ここでは、cache_controlヘッダを設定して、ブラウザがキャッシュをどの程度の期間保持すべきかを指示している。ここでは最大365日間キャッシュを保持するように設定。
+end
