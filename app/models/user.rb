ec6f6eba073c787459c7584c6ba9e8a2573e0aac
Akira <akisasa1994@gmail.com>
2024-01-17 10:29:42 +0900

    seedファイルで初期データ準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Categories
+1.times do
+    Category.create!(name: "オフィス")  #id:1
+    Category.create!(name: "在宅")     #id:2
+end
+
+# Places
+1.times do
+    Place.create!(name: "フリーアドレス", category_id: 1)
+    Place.create!(name: "固定席", category_id: 1)
+    Place.create!(name: "自室", category_id: 2)
+    Place.create!(name: "リビング", category_id: 2)
+    Place.create!(name: "ダイニング", category_id: 2)
+    Place.create!(name: "書斎", category_id: 2)
+    Place.create!(name: "その他", category_id: 2)
+end
