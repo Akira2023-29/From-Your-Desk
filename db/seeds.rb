7f1c9455401ed343883899c0945fe3687b2e2cda
Akira <akisasa1994@gmail.com>
2024-01-17 10:59:43 +0900

    seedファイルの初期データリセット

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-# Categories
-1.times do
-    Category.create!(name: "オフィス")  #id:1
-    Category.create!(name: "在宅")     #id:2
-end
+Category.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('categories') # idのカウンターをリセット
+Category.create!(id: 1, name: "オフィス")  # id:1
+Category.create!(id: 2, name: "在宅")     # id:2
 
-# Places
-1.times do
-    Place.create!(name: "フリーアドレス", category_id: 1)
-    Place.create!(name: "固定席", category_id: 1)
-    Place.create!(name: "自室", category_id: 2)
-    Place.create!(name: "リビング", category_id: 2)
-    Place.create!(name: "ダイニング", category_id: 2)
-    Place.create!(name: "書斎", category_id: 2)
-    Place.create!(name: "その他", category_id: 2)
-end
+Place.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('places') # idのカウンターをリセット
+Place.create!(name: "フリーアドレス", category_id: 1)
+Place.create!(name: "固定席", category_id: 1)
+Place.create!(name: "自室", category_id: 2)
+Place.create!(name: "リビング", category_id: 2)
+Place.create!(name: "ダイニング", category_id: 2)
+Place.create!(name: "書斎", category_id: 2)
+Place.create!(name: "その他", category_id: 2)
