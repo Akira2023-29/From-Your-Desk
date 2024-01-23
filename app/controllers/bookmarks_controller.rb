da497ca13fc0055e014a6fe8a41ca13642462265
Akira <akisasa1994@gmail.com>
2024-01-23 23:39:06 +0900

    bookmrksコントローラーのアクション実装

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,11 @@
 class BookmarksController < ApplicationController
   def create
+    @item = Item.find(params[:item_id])
+    current_user.bookmark(@item)
   end
 
   def destroy
+    @item = current_user.bookmark.find(params[:id]).item
+    current_user.unbookmark(@item)
   end
 end
