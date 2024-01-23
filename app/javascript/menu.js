58a942a44cc7351bacb86e5957a213cff1759578
Akira <akisasa1994@gmail.com>
2024-01-23 17:55:38 +0900

    item関連ヘッダーメニュー作成

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,19 +1,38 @@
 document.addEventListener('turbo:load', (event) => {
     const menuButton = document.getElementById('menu-button');
-    const menu = document.querySelector('[role="menu"]');
+    const diagnosisMenuButton = document.getElementById('diagnosis_menu-button');
+    const itemMenuButton = document.getElementById('item_menu-button');
+    const menu = document.querySelector('[aria-labelledby="menu-button"]');
+    const diagnosisMenu = document.querySelector('[aria-labelledby="diagnosis_menu-button"]');
+    const itemMenu = document.querySelector('[aria-labelledby="item_menu-button"]');
 
-    if (menuButton) {
-        // 初期状態ではメニューを非表示にする
+    if (menuButton && menu) {
         menu.style.display = 'none';
+        menuButton.addEventListener('click', (event) => {
+            menu.style.display = (menu.style.display === 'none') ? 'block' : 'none';
+            event.stopPropagation();
+        });
+    };
 
-        // メニューボタンがクリックされたときのイベントリスナーを設定
-        menuButton.addEventListener('click', () => {
-            // メニューが表示されている場合は非表示にし、非表示の場合は表示する
-            if (menu.style.display === 'none') {
-                menu.style.display = 'block';
-            } else {
-                menu.style.display = 'none';
-            }
+    if (diagnosisMenuButton && diagnosisMenu) {
+        diagnosisMenu.style.display = 'none';
+        diagnosisMenuButton.addEventListener('click', (event) => {
+            diagnosisMenu.style.display = (diagnosisMenu.style.display === 'none') ? 'block' : 'none';
+            event.stopPropagation();
+        });
+    };
+
+    if (itemMenuButton && itemMenu) {
+        itemMenu.style.display = 'none';
+        itemMenuButton.addEventListener('click', (event) => {
+            itemMenu.style.display = (itemMenu.style.display === 'none') ? 'block' : 'none';
+            event.stopPropagation();
         });
     }
+
+    document.addEventListener('click', () => {
+        if (menu) menu.style.display = 'none';
+        if (diagnosisMenu) diagnosisMenu.style.display = 'none';
+        if (itemMenu) itemMenu.style.display = 'none';
+    });
 });
