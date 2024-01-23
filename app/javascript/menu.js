32857e5ca38dfdbbfd2ed7b8c8ce134ceebc8b1c
Akira <akisasa1994@gmail.com>
2024-01-23 20:57:52 +0900

    ヘッダーレスポンシブ対応

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -2,37 +2,48 @@ document.addEventListener('turbo:load', (event) => {
     const menuButton = document.getElementById('menu-button');
     const diagnosisMenuButton = document.getElementById('diagnosis_menu-button');
     const itemMenuButton = document.getElementById('item_menu-button');
+    const smallMenuButton = document.getElementById('small_menu-button');
+
     const menu = document.querySelector('[aria-labelledby="menu-button"]');
     const diagnosisMenu = document.querySelector('[aria-labelledby="diagnosis_menu-button"]');
     const itemMenu = document.querySelector('[aria-labelledby="item_menu-button"]');
+    const smallMenu = document.querySelector('[aria-labelledby="small_menu-button"]');
 
-    if (menuButton && menu) {
-        menu.style.display = 'none';
-        menuButton.addEventListener('click', (event) => {
-            menu.style.display = (menu.style.display === 'none') ? 'block' : 'none';
-            event.stopPropagation();
-        });
+    const toggleMenu = (button, menu) => {
+        if (button && menu) {
+            menu.style.display = 'none';
+            let isHovering = false;
+
+            const openMenu = () => {
+                isHovering = true;
+                menu.style.display = 'block';
+            };
+
+            const closeMenu = () => {
+                isHovering = false;
+                setTimeout(() => {
+                    if (!isHovering) {
+                        menu.style.display = 'none';
+                    }
+                }, 10);
+            };
+
+            button.addEventListener('mouseover', openMenu);
+            button.addEventListener('mouseout', closeMenu);
+            menu.addEventListener('mouseover', openMenu);
+            menu.addEventListener('mouseout', closeMenu);
+        }
     };
 
-    if (diagnosisMenuButton && diagnosisMenu) {
-        diagnosisMenu.style.display = 'none';
-        diagnosisMenuButton.addEventListener('click', (event) => {
-            diagnosisMenu.style.display = (diagnosisMenu.style.display === 'none') ? 'block' : 'none';
-            event.stopPropagation();
-        });
-    };
-
-    if (itemMenuButton && itemMenu) {
-        itemMenu.style.display = 'none';
-        itemMenuButton.addEventListener('click', (event) => {
-            itemMenu.style.display = (itemMenu.style.display === 'none') ? 'block' : 'none';
-            event.stopPropagation();
-        });
-    }
+    toggleMenu(menuButton, menu);
+    toggleMenu(diagnosisMenuButton, diagnosisMenu);
+    toggleMenu(itemMenuButton, itemMenu);
+    toggleMenu(smallMenuButton, smallMenu);
 
     document.addEventListener('click', () => {
         if (menu) menu.style.display = 'none';
         if (diagnosisMenu) diagnosisMenu.style.display = 'none';
         if (itemMenu) itemMenu.style.display = 'none';
+        if (smallMenu) smallMenu.style.display = 'none';
     });
 });
