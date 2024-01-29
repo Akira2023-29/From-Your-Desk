ccbe4297bfc61bd9549ba9ce82ca12ca72830a5d
Akira <akisasa1994@gmail.com>
2024-01-29 11:00:19 +0900

    jsファイルの挙動修正

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ApplicationRecord
     belongs_to :category
     has_many :diagnoses, dependent: :destroy
 
+    validates :category_id, presence: true
+
     # 検索条件で使えるカラムを指定
     def self.ransackable_attributes(auth_object = nil)
         %w[category_id]
