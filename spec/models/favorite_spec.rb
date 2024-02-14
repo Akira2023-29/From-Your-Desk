bfbe26839af59c56158130b560b2c603ef5278fb
Akira <akisasa1994@gmail.com>
2024-02-14 14:31:55 +0900

    Add: favoriteモデルのテストケース洗い出し

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+    context '正常系' do
+        it '全てのフィールドが正常な場合、有効であること' do
+            # favorite = build(:favorite)
+            # expect(favorite).to be_valid
+        end
+    end
+
+    context '異常系' do
+        it 'ユーザーと掲示板の組み合わせがユニークでない場合,無効であること' do
+            # favotite = create(:favorite)
+            # new_favorite = build(:favorite, user: favorite.user, board: favorite.board)
+            # new_favorite.valid?
+            # expect(new_favorite.errors[:user_id]).to include('はすでに存在します'), 'favoriteとuserのユニークバリデーションが設定されていません'
+        end
+    end
+end
