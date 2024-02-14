206ffc39c7f10a02c235c1c105468e7292b9b238
Akira <akisasa1994@gmail.com>
2024-02-14 12:19:41 +0900

    Add: Modelスペック（User）実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { "Akira" }
     sequence(:email) { |n| "rspec_#{n}@example.com" }
     password { "password" }
-    password_confirmation { "password_confirmation" }
+    password_confirmation { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   # pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+    it '名前、メールがあり、パスワードは6文字以上であれば有効であること' do
+        user = build(:user)
+        expect(user).to be_valid
+    end
+
+    it 'メールはユニークてあること' do
+        user1 = create(:user)
+        user2 = build(:user)
+        user2.email = user1.email
+        user2.valid?
+        expect(user2.errors[:email]).to include('はすでに存在します')
+    end
+
+    it '名前、メールアドレスは必須項目であること' do
+        user = build(:user)
+        user.email = nil
+        user.name = nil
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+        expect(user.errors[:name]).to include('を入力してください')
+    end
+
+    it '名前は255文字以下であること' do
+        user = build(:user)
+        user.name = 'a' * 256
+        user.valid?
+        expect(user.errors[:name]).to include('は255文字以内で入力してください')
+    end
+end
