42e485b62a6541b38653740fcdb5f3186dc56876
Akira <akisasa1994@gmail.com>
2024-02-14 14:26:23 +0900

    Add: bookmarkモデルのテストケース洗い出し

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe Bookmark, type: :model do
+    context '正常系' do
+        it '全てのフィールドが正常な場合、有効であること' do
+            # bookmark = build(:bookmark)
+            # expect(bookmark).to be_valid
+        end
+    end
+
+    context '異常系' do
+        it 'ユーザーと掲示板の組み合わせがユニークでない場合,無効であること' do
+            # bookmark = create(:bookmark)
+            # new_bookmark = build(:bookmark, user: bookmark.user, board: bookmark.board)
+            # new_bookmark.valid?
+            # expect(new_bookmark.errors[:user_id]).to include('はすでに存在します'), 'bookmarkとuserのユニークバリデーションが設定されていません'
+        end
+    end
+end
